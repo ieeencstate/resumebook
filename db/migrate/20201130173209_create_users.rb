3dde6a62ef9ca7045c1ae0250d6b64c8210c73f2
John Bumgardner <john.bumgardner@icloud.com>
2020-11-30 15:20:07 -0500

    added variable to separate student and employer

--- db/migrate/20201130173209_create_users.rb
+++ db/migrate/20201130173209_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-
+      t.string :user_type
       t.timestamps
     end
   end
